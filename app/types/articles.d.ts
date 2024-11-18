2e3d67cdd66b9955a9ccf04672599b938afaf75c
LittiosiN <joaquinabelleira@gmail.com>
2024-11-18 15:45:38 -0300

    uploading type

--- app/types/articles.d.ts
+++ app/types/articles.d.ts
@@ -1,13 +1,21 @@
 import { Section } from "./sections"
 
-export type Article = {
+export type ImageTpe = {
+  src: string
+  caption: string
+}
+
+export type MainImageType = ImageTpe & {srcSEO: string}
+
+export type ArticleType = {
   _id: string
   title:string
   slug:string
   content:string
   articleId:number
-  subhead:string
-  volanta:string
+  subhead?:string
+  volanta?:string
   lastModifiedBy:string
   section:Section
+  image?: MainImageType
 }

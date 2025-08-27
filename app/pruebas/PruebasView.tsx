dcf776f38257d14f65bfc09a757dd72a7ddfb3c9
LittiosiN <joaquinabelleira@gmail.com>
2025-08-27 11:20:15 -0300

    removing async from client component for the moment not acepted

--- app/pruebas/PruebasView.tsx
+++ app/pruebas/PruebasView.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 const Editor = dynamic(() => import('../components/inputs/Editor'), { ssr: false });
 //import Editor from "../components/inputs/Editor";
 
-const PruebasView = async () => {
+const PruebasView = () => {
    return (
     <Editor initial={[]} onChange={(html) => console.log("html", html)}/>
   )
